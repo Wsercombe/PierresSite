428c4b34617696d003b21ceaf056d108110a94f6
William Sercombe <wsercombe@gmail.com>
2021-11-08 19:34:37 -0800

    Add treat model

---
+++ PierresSite/Models/Treat.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace PierresSite.Models
+{
+  public class Treat
+  {
+    public Treat()
+    {
+      this.JoinEntities = new HashSet<FlavorTreat>();
+    }
+
+    public int TreatId { get; set; }
+    public string Name { get; set; }
+    public virtual ICollection<FlavorTreat> JoinEntities { get; set; }
+  }
+}
